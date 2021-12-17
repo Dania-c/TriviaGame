48627289a7026ec634de7192d6399e87732e8e00
dania-c <workingdania@hotmail.com>
2021-12-17 22:31:53 +0200

    little tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 const API_URL = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 function App() {
   return (
-    <h1 >
-      hello ...
-    </h1>
+    <div className="container">
+      <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md"> <h2 className="text-2xl">question goes here</h2> </div>
+      <div className="flex flex-wrap mt-4 justify-around ">
+        <button className="bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow mb-4">option 1</button>
+        <button className="bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow mb-4">option 2</button>
+        <button className="bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow">option 3</button>
+        <button className="bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow">option 4</button>
+      </div>
+
+    </div>
   );
 }
 
