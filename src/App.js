d3bbf1b2d107b52465d19a407e43bdf95d75f857
dania-c <workingdania@hotmail.com>
2021-12-20 11:44:38 +0200

    currentIndex

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Questionaire from "./components/Questionaire";
 const API_URL = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 function App() {
   const [questions, setQuestions] = useState([])
-
+  const [currentIndex, setCurrentIndex] = useState(0)
   useEffect(() => {
     fetch(API_URL)
       .then(res => res.json())
@@ -12,10 +12,13 @@ function App() {
         console.log(data.results);
         setQuestions(data.results);
         console.log("q:", questions);
+        // setCurrentIndex(data.results[0])
       })
   }, []);
 
   const handleAnswer = (answer) => {
+    setCurrentIndex(currentIndex + 1);
+
 
   }
 
@@ -23,9 +26,14 @@ function App() {
     <>
 
 
-      {questions.length > 0 ? (
-        <Questionaire data={questions[0]} handleAnswer={handleAnswer} />
-      ) : (<h3 className="text-2xl text-white font-bold"> Loading...</h3>)}
+      {questions.length > 0 ?
+        (
+
+          <div className="container">
+            <Questionaire data={questions[currentIndex]} handleAnswer={handleAnswer} />
+          </div>
+        ) :
+        (<h3 className="text-2xl text-white font-bold"> Loading...</h3>)}
 
 
 
